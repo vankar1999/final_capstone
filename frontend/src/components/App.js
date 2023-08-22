d5e18c76923e72901e80d921c99f8f518185106a
Jennifer-Tsang_lmig <Jennifer.Tsang@LibertyMutual.com>
2023-08-22 15:17:17 -0400

    added the  base on the Home page

---
+++ frontend/src/components/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import Navbar from '/NavBar';
+// import CurrentPage from './CurrentPage';
+import Home from './Home';
+// import Mountains from './Mountains';
+// import Parks from './Parks';
+
+
+function App() {
+  return (
+    
+    <div>
+      <Home/>
+    </div>
+    
+ );
+}
+
+export default App;
