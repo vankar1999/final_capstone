58c0ca31207e85296dd6d36daac9151a6aebccb5
Sathambakkam <karunakaran.sathambakkam@libertymutual.com>
2023-08-22 10:57:17 -0700

    initial react project

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>This is a starter page</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
