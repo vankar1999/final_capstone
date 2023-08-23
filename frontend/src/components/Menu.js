0eb7f45b0a50bda3b04969392d5dfb9d58133398
Jennifer-Tsang_lmig <Jennifer.Tsang@LibertyMutual.com>
2023-08-23 13:36:57 -0400

    add drop dropdown on the menu page, non functional yet

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -1,22 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 // import './Menu.css'
 
 
 export default function Menu() {
-      return (
-        <>
-          <div>
-            <h1>Menu</h1>
-          </div>
-          <div>
-            <img src="https://imagedelivery.net/WLUarKbmUXuuhDC7PG5_Qw/seo/cad39ed25806987c88d2881af39a130e.jpg/public" alt="food"/>
-            
-            <div className="menuDescription">
-                <p>
-                Select by category 
-                </p>
-            </div>
-        </div>
-      </>
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  const categories = ['Appetizer', 'Entree', 'Dessert'];
+
+  return (
+    <div>
+      <h1>Menu</h1>
+      <img src="https://imagedelivery.net/WLUarKbmUXuuhDC7PG5_Qw/seo/cad39ed25806987c88d2881af39a130e.jpg/public" alt="food"/>
+      <form>
+        <select onChange={(e) => {
+          const c = categories?.find((x) => x === e.target.value);
+          setSelectedCategory(c);
+        }}>
+          <option disabled="" value="">Select menu category</option>
+          {categories.map(category => (
+            <option key={category} value={category}>{category}</option>
+          ))}
+        </select>
+      </form>
+    </div>
     )
   }
