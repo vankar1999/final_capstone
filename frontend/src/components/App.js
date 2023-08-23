e278191ef64326328c1814f6bede16145ffb3855
Sathambakkam <karunakaran.sathambakkam@libertymutual.com>
2023-08-23 09:41:53 -0700

    added the navbar code

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,18 +1,32 @@
 import './App.css';
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import Navbar from '/NavBar';
-// import CurrentPage from './CurrentPage';
+import NavbarCap from './NavbarCap';
+import CurrentPage from './CurrentPage';
 import Home from './Home';
 // import Mountains from './Mountains';
 // import Parks from './Parks';
 
 
 function App() {
+  const [currentPageName, setCurrentPageName] = useState("home")
+  const handleHome = () => {setCurrentPageName("home")}
+
+  let currentPage = null;
+  switch(currentPageName){
+    case "home": 
+      currentPage = <Home/>;
+      break;
+    //placeholder for other pages
+    default: <Home/>;
+  }
   return (
     
     <div>
-      <Home/>
+      <NavbarCap  handleHome={handleHome} /> 
+      <div>
+        <CurrentPage currentPage={currentPage} />
+      </div>
     </div>
     
  );
