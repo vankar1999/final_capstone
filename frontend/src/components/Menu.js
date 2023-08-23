0c2d6a12ec3146b7d5e74ca424ad8c346434922e
Jennifer-Tsang_lmig <Jennifer.Tsang@LibertyMutual.com>
2023-08-23 16:52:00 -0400

    added placeholder menu item card

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import Card from 'react-bootstrap/Card';
 // import './Menu.css'
 
 
@@ -22,6 +23,21 @@ export default function Menu() {
           ))}
         </select>
       </form>
+
+      <div className="mountain-card">
+            <Card style={{ width: '30rem' }}>
+                <Card.Img variant="top" src="https://upload.wikimedia.org/wikipedia/commons/a/a0/Chicken_lollipop_in_Goa.jpg" alt="picture of chicken lollipop" />
+                <Card.Body>
+                <Card.Title>Chicken Lollipop</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Price: $10.99</Card.Subtitle>
+                <Card.Text>
+                    This is a chicken lollipop.
+                </Card.Text>
+                </Card.Body>
+            </Card>
+        </div>
+
+
     </div>
     )
   }
