211a7e2526b90ffd01d335ff5cf954acab63addf
Sathambakkam <karunakaran.sathambakkam@libertymutual.com>
2023-08-24 08:12:44 -0700

    added menu components

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Card from 'react-bootstrap/Card';
-// import './Menu.css'
+import MenuDetails from './MenuDetails'
 
 
 export default function Menu() {
@@ -11,7 +11,7 @@ export default function Menu() {
   return (
     <div>
       <h1>Menu</h1>
-      <img src="https://imagedelivery.net/WLUarKbmUXuuhDC7PG5_Qw/seo/cad39ed25806987c88d2881af39a130e.jpg/public" alt="food"/>
+      <img src="https://imagedelivery.net/WLUarKbmUXuuhDC7PG5_Qw/seo/cad39ed25806987c88d2881af39a130e.jpg/public" alt="food" />
       <form>
         <select onChange={(e) => {
           const c = categories?.find((x) => x === e.target.value);
@@ -24,20 +24,11 @@ export default function Menu() {
         </select>
       </form>
 
-      <div className="mountain-card">
-            <Card style={{ width: '30rem' }}>
-                <Card.Img variant="top" src="https://upload.wikimedia.org/wikipedia/commons/a/a0/Chicken_lollipop_in_Goa.jpg" alt="picture of chicken lollipop" />
-                <Card.Body>
-                <Card.Title>Chicken Lollipop</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">Price: $10.99</Card.Subtitle>
-                <Card.Text>
-                    This is a chicken lollipop.
-                </Card.Text>
-                </Card.Body>
-            </Card>
-        </div>
+      <div >
+        <MenuDetails />
+      </div>
 
 
     </div>
-    )
-  }
+  )
+}
