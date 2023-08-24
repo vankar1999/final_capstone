7ef87b1f5453c284a6b67c03e976952eb9dc95db
Sathambakkam <karunakaran.sathambakkam@libertymutual.com>
2023-08-24 09:30:21 -0700

    added Add button and updated styling on the menu page

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Card from 'react-bootstrap/Card';
 import MenuDetails from './MenuDetails'
+import './Menu.css'
 
 
 export default function Menu() {
@@ -12,7 +13,10 @@ export default function Menu() {
     <div>
       <h1>Menu</h1>
       <img src="https://imagedelivery.net/WLUarKbmUXuuhDC7PG5_Qw/seo/cad39ed25806987c88d2881af39a130e.jpg/public" alt="food" />
-      <form>
+
+      {/* This is going to be the dropdown part of the page */}
+
+      <form >
         <select onChange={(e) => {
           const c = categories?.find((x) => x === e.target.value);
           setSelectedCategory(c);
@@ -24,11 +28,13 @@ export default function Menu() {
         </select>
       </form>
 
+      <div class="add-button">
+        <button type="button" class="btn add-btn">Add New Item</button>
+      </div>
+
       <div >
         <MenuDetails />
       </div>
-
-
     </div>
   )
 }
