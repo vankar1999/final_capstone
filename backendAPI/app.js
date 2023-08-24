bf1fee4b64a075e90f85ef62e3a645a3b10d8230
Jennifer-Tsang_lmig <Jennifer.Tsang@LibertyMutual.com>
2023-08-24 16:41:42 -0400

    connect to the DB and set up GET

--- backendAPI/app.js
+++ backendAPI/app.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const cors = require('cors')
 
 const app = express()
+
+const Menuitems = require('./models/menuitems')
 
 const port = process.env.PORT || 5000
 
@@ -9,10 +12,15 @@ app.listen(port, () =>{
     console.log(`app listening on port ${port}`)
 })
 
-app.get("/",(request,response) =>{
-    response.json({
-        "message": "Hello World"
-    })
+// const menu = mongoose.model("menu",{    category: { type: String },
+// itemName: { type: String },
+// ingredients: { type: String },
+// price: { type: String },
+// img: { type: String }})
+
+app.get("/menuitems", async (req, res) => {
+    const menu2 = await Menuitems.find() 
+    res.json(menu2);
 })
 
 app.use(cors());
@@ -22,3 +30,10 @@ app.use(function (req, res) {
     err.status = 404
     res.json(err)
 })
+
+mongoose.connect("mongodb://127.0.0.1:27017/menu", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+})
+    .then(() => console.log("Connected to DB"))
+    .catch(console.error);
--- backendAPI/models/menuitems.js
+++ backendAPI/models/menuitems.js
@@ -1,13 +1,13 @@
-const mongoose = require(‘mongoose’)
+const mongoose = require('mongoose')
 
 const menuItemsSchema = new mongoose.Schema({
 
-category: { type: String },
-itemName: { type: String },
-ingredients: { type: String },
-price: { type: String },
-img: { type: String }
+    category: { type: String },
+    itemName: { type: String },
+    ingredients: { type: String },
+    price: { type: String },
+    img: { type: String }
 
 })
 
-module.exports = mongoose.model(‘Menuitems’, menuItemsSchema)
+module.exports = mongoose.model('Menuitems', menuItemsSchema)
