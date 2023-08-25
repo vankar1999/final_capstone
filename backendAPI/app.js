71de359dc15b2c6e40480b0149ec87d05966006a
Sathambakkam <karunakaran.sathambakkam@libertymutual.com>
2023-08-25 12:07:47 -0700

    added post method code'

--- backendAPI/app.js
+++ backendAPI/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const router = require('express').Router()
 
 const app = express()
 app.use(cors());
@@ -19,6 +20,28 @@ app.get("/menuitems", async (req, res) => {
     res.json(menu2);
 })
 
+router.route('/add').post((req,res) => {
+    const category = req.body.category;
+    const itemname = req.body.itemname;
+    const ingredients = req.body.ingredients;
+    const price = req.body.price;
+    const img = req.body.img;
+
+} )
+
+const newMenuItems = new Menuitems({
+    category,
+    itemname,
+    ingredients,
+    price,
+    img,
+});
+
+newMenuItems.save()
+    .then(() => res.json('Menu Added'))
+    .catch(err => res.status(400).json('Error: ' + err));
+
+
 app.use(function (req, res) {
     const err = new Error('Not Found')
     err.status = 404
@@ -31,3 +54,5 @@ mongoose.connect("mongodb://127.0.0.1:27017/menu", {
 })
     .then(() => console.log("Connected to DB"))
     .catch(console.error);
+
+module.exports = router;
