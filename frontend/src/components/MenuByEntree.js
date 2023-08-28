5f941d7c0da5cf50a15cf4c1613d0be44a176a8e
Jennifer-Tsang_lmig <Jennifer.Tsang@LibertyMutual.com>
2023-08-28 13:37:51 -0400

    update styling on the menu items

--- frontend/src/components/MenuByEntree.js
+++ frontend/src/components/MenuByEntree.js
@@ -14,20 +14,23 @@ export default function MenuByEntree({ selectedCategory, menuItems }) {
             <>
                 <AddButton />
             </> 
-            <div className="menu-card">
+            <div className="menu-card row row-cols-2 row-cols-lg-5 row-cols-md-4 row-cols-sm-3 g-4">
                 {menuByEntree.map((item) => (
-                <Card style={{ width: '20rem' }} key={item._id}>
-                    <Card.Img variant="top" src={`/assets/images/${item.img}`} alt={`picture of ${item.itemName}`} />
+                <Card style={{ width: '20rem' }} key={item._id}className = "card2 h-150">
+                    <Card.Img className="card-image" variant="top" src={`/assets/images/${item.img}`} alt={`picture of ${item.itemName}`} />
                     <Card.Body>
-                    <Card.Title>{item.itemName}</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">Price: {item.price}</Card.Subtitle>
-                    <Card.Text>
-                        {item.ingredients}
-                    </Card.Text>
-                        <EditButton />
-                        <DeleteButton />
+                        <Card.Title>{item.itemName}</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">Price: {item.price}</Card.Subtitle>
+                        <Card.Text>
+                            {item.ingredients}
+                        </Card.Text>
                     </Card.Body>
-
+                    <div className="card-footer">
+                        <div>
+                            <EditButton />
+                            <DeleteButton />
+                        </div>
+                    </div>
                 </Card>
                 ))}
             </div>
